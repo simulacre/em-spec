6217643795ff0e718515907ed4e6060fed1f3500
Daniel DeLeo <danielsdeleo@mac.com>
2009-04-22 17:27:23 -0600

    clean up leftovers from interim API

--- test/rspec_fail_examples.rb
+++ test/rspec_fail_examples.rb
@@ -5,12 +5,10 @@ describe EventMachine, "when running failing examples" do
   include EMSpec
   
   it "should not bubble failures beyond rspec" do
-    #em do
-      EM.add_timer(0.1) do
-        :should_not_bubble.should == :failures
-        done
-      end
-    #end
+    EM.add_timer(0.1) do
+      :should_not_bubble.should == :failures
+      done
+    end
   end
 
   it "should not block on failure" do
